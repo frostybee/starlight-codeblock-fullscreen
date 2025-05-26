8f44dd2045918727d692f340239f083922bd6b20
frostybee <rsleiman2@gmail.com>
2025-05-26 14:37:03 -0400

    Update docs

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
           fullscreenButtonTooltip: 'Toggle fullscreen view',
           enableEscapeKey: true,
           exitOnBrowserBack: true,
-          addToFramelessBlocks: false,
+          addToFramelessBlocks: true,
           fullscreenZoomLevel: 150,
           animationDuration: 200,
           svgPathFullscreenOn: "M16 3h6v6h-2V5h-4V3zM2 3h6v2H4v4H2V3zm18 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z",
@@ -35,10 +35,11 @@ export default defineConfig({
           ],
         },
         {
-          label: 'Features',
+          label: 'Features & Examples',
           collapsed: false,
           items: [
             { slug: 'features' },
+            { slug: 'examples' },
           ],
         }
       ],
